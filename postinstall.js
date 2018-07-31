7266d1fdddf99247427c06338de1d99b2fa7edfe
nkasprak <nkasprak@cbpp.org>
2018-07-31 13:58:01 -0400

    minify here to prevent browserify confusion

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,13 @@
 const babelify = require("babelify");
 const browserify = require("browserify");
 const fs = require("fs");
+const uglify = require("uglify-js");
 browserify("./main.js")
   .transform(babelify, {presets:["env"]})
   .bundle()
-  .pipe(fs.createWriteStream("./main_trans.js"));
+  .pipe(fs.createWriteStream("./main_trans.js"))
+  .on("finish", ()=> {
+    var f = fs.readFileSync("./main_trans.js","utf-8");
+    var uglified = uglify.minify(f);
+    fs.writeFileSync("./main_dist.js", uglify.minify(f).code);
+  });
