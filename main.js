e91fc6becfe58f82bc0823cd027dd52765cb1288
Nicholas Kasprak <nkasprak@gmail.com>
2024-10-16 12:30:25 -0400

    allow lab color option

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ const HSLtoRGB = require("hsl-to-rgb-for-reals");
 const RGBtoHSL = require("rgb-to-hsl");
 const HexToRGB = require("hex-rgb");
 const RGBToHex = require("rgb-hex");
+const { rgb2lab, lab2rgb } = require("rgb-lab");
 
 /*var HSLtoRGB = RGBtoHSL = function(r, g, b) {
   return [r, g, b];
 };*/
 
-module.exports = function(start, end, n, curve) {
+module.exports = function(start, end, n, curve, uselab) {
   var rgb;
-  if (typeof(curve)==="undefined") {
+  if (typeof(curve)!=="function") {
     curve = function(n) {return n;};
   }
   var is_hex = false;
@@ -24,23 +25,34 @@ module.exports = function(start, end, n, curve) {
     is_hex = true;
   }
   var p;
-  var startHSL = noPercent(RGBtoHSL.apply(null, start));
-  var endHSL = noPercent(RGBtoHSL.apply(null, end));
-  if (startHSL[0] - endHSL[0] < -180) {
-    startHSL[0] += 360;
-  } else if (startHSL[0] - endHSL[0] > 180) {
-    endHSL[0] += 360;
+  var startTargetSpace, endTargetSpace;
+  if (uselab) {
+    startTargetSpace = rgb2lab(start);
+    endTargetSpace = rgb2lab(end);
+  } else {
+    startTargetSpace = noPercent(RGBtoHSL.apply(null, start));
+    endTargetSpace = noPercent(RGBtoHSL.apply(null, end));
+    if (startTargetSpace[0] - endTargetSpace[0] < -180) {
+      startTargetSpace[0] += 360;
+    } else if (startTargetSpace[0] - endTargetSpace[0] > 180) {
+      endTargetSpace[0] += 360;
+    }
   }
+  
   var test = [];
   var r = [];
   for (var i = 0; i<n; i++) {
     p = curve(i/(n-1));
     var c = [];
     for (var j = 0; j<3;j++) {
-      c[j] = (endHSL[j]-startHSL[j])*p + startHSL[j];
+      c[j] = (endTargetSpace[j]-startTargetSpace[j])*p + startTargetSpace[j];
     }
-    c[0] = (c[0]+360)%360;
-    rgb = HSLtoRGB.apply(null, c);
+    if (!uselab) {
+      c[0] = (c[0]+360)%360;
+      rgb = HSLtoRGB.apply(null, c);
+    } else {
+      rgb = lab2rgb(c);
+    }
     if (is_hex) {
       r.push("#" + RGBToHex(rgb[0], rgb[1], rgb[2]));
     } else {
